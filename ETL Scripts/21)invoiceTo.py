3d9d330e0bb6797d40d0d9b672825663fd7d8c6f
sakthi <sakthi.krishna@outlook.com>
2025-07-04 11:37:26 +0100

    finished till tax

---
+++ ETL Scripts/21)invoiceTo.py
@@ -0,0 +1,41 @@
+from libs import *
+from src.utils import get_tgt_myconnection, get_src_accessdb_connection, get_src_accessdb2_connection, safe_value
+
+myconnection = get_tgt_myconnection()
+target_cursor = myconnection.cursor()
+
+warnings.filterwarnings("ignore")
+
+src_billTo_df = pd.DataFrame(
+    ('Hospital','Specalist'),
+    columns=['billTo']
+)
+
+#---------------------id generation-------------------
+billTo_max = 'SELECT MAX(id) FROM bill_to'
+billTo_max_df = pd.read_sql(billTo_max, myconnection)
+if billTo_max_df is None or billTo_max_df.iloc[0, 0] is None:
+    max_id = 1
+else:
+    max_id = billTo_max_df.iloc[0, 0] + 1
+src_billTo_df.insert(0, 'bill_to_id', range(max_id, max_id + len(src_billTo_df)))
+
+#-----------------------inserting billTo into target database---------------------
+bar = tqdm(total=len(src_billTo_df), desc='Inserting Bill To')
+
+for index,row in src_billTo_df.iterrows():
+    bar.update(1)
+    try:
+        billTo_query = f"""
+        INSERT INTO `bill_to` (`id`, `name`, `order`, `is_default`, `created_at`, `updated_at`, `deleted_at`) 
+        VALUES ({safe_value(row['bill_to_id'])}, {safe_value(row['billTo'])}, {safe_value(row['bill_to_id'])}, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL); 
+        """
+        target_cursor.execute(billTo_query)
+    except Exception as e:
+        logging.error(f"Error inserting row {index}: {e}")
+        break
+
+myconnection.commit()
+myconnection.close()
+bar.close()
+print("Bill To data inserted successfully.")
---
+++ ETL Scripts/7)hospitalFromInvoices.py
@@ -0,0 +1,75 @@
+from libs import *
+from src.utils import get_tgt_myconnection, get_src_accessdb_connection, get_src_accessdb2_connection, safe_value
+
+myconnection = get_tgt_myconnection()
+target_cursor = myconnection.cursor()
+
+warnings.filterwarnings("ignore")
+
+src_invoice_hospitals  = 'SELECT * FROM InvoiceHeadSummary WHERE InvoiceTo = 3'
+src_invoice_hospitals_df = pd.read_sql(src_invoice_hospitals, get_src_accessdb_connection())
+
+#--------------------------Dropping rows when Account Name and PatientName is equal----------------
+#src_invoice_hospitals_df['patientName'] = src_invoice_hospitals_df.apply(lambda x: x['Title'] + ' ' + x['Initials'] + ' ' + x['LastName'], axis=1)
+src_invoice_hospitals_df['patientLastName'] = src_invoice_hospitals_df.apply(lambda x: x['LastName'], axis=1)
+src_invoice_hospital_df1 = src_invoice_hospitals_df[~
+    src_invoice_hospitals_df.apply(
+        lambda x: pd.notnull(x['AccountName']) and pd.notnull(x['patientLastName']) and x['patientLastName'].lower() in x['AccountName'].lower(),
+        axis=1
+    )
+]
+src_invoice_hospital_df101 = (
+    src_invoice_hospital_df1[['AccountName']]
+    .assign(AccountName_Upper=src_invoice_hospital_df1['AccountName'].str.upper())
+    .drop_duplicates(subset=['AccountName_Upper'])
+    .reset_index(drop=True)
+)
+
+src_invoice_hospital_df2 = src_invoice_hospital_df101
+
+tgt_hospitals = 'SELECT DISTINCT name,UPPER(LTRIM(RTRIM(name))) as AccountName_Upper FROM hospitals'
+tgt_hospitals_df = pd.read_sql(tgt_hospitals, myconnection)
+
+#--------------------------Filtering out hospitals that already exist in the target--------------------------
+src_invoice_hospital_dfx= src_invoice_hospital_df2[
+    ~src_invoice_hospital_df2['AccountName_Upper'].isin(tgt_hospitals_df['AccountName_Upper'])
+]
+src_invoice_hospital_dfx = src_invoice_hospital_dfx.drop(columns=['AccountName_Upper']).reset_index(drop=True)
+
+#--------------------------Adding Source identifier column in target------------------------------------------
+query_1 = "SET sql_mode = ''"
+target_cursor.execute(query_1)
+query_2 = "ALTER TABLE hospitals ADD COLUMN IF NOT EXISTS PPM_InvoiceHospital_Id VARCHAR(100) DEFAULT NULL;"
+target_cursor.execute(query_2)
+myconnection.commit()
+
+#--------------------------hospital_id generation-----------------------------------------------------------
+hospital_max = 'SELECT MAX(id) FROM hospitals'
+hospital_max_df = pd.read_sql(hospital_max, myconnection)
+if hospital_max_df is None or hospital_max_df.iloc[0, 0] is None:
+    max_id = 1
+else:
+    max_id = hospital_max_df.iloc[0, 0] + 1
+src_invoice_hospital_dfx.insert(0, 'hospital_id', range(max_id, max_id + len(src_invoice_hospital_dfx)))
+
+#----------------------------Archiving the records which doesn't have hospital in name----------------------
+src_invoice_hospital_dfx['is_archive'] = (~src_invoice_hospital_dfx['AccountName'].str.lower().str.contains('hospital')).astype(int)
+
+bar = tqdm(total=len(src_invoice_hospital_dfx), desc='Inserting Hospitals from InvoiceHeadSummary', position=0)
+
+for index, row in src_invoice_hospital_dfx.iterrows():
+    bar.update(1)
+    try:
+        query = f"""
+        INSERT INTO `hospitals` (`id`, `name`, `type_id`, `income_category_id`, `address1`, `address2`, `address3`, `town`, `county`, `postcode`, `phone`, `mobile`, `fax`, `email`, `website`, `latitude`, `longitude`, `color_code`, `form_template`, `is_default_clinic`, `is_default_hospital`, `is_archive`, `health_claim_hospital_code`, `created_user_id`, `updated_user_id`, `deleted_user_id`, `created_at`, `updated_at`, `deleted_at`, `PPM_InvoiceHospital_Id`) 
+        VALUES ({safe_value(row['hospital_id'])}, {safe_value(row['AccountName'])}, 4, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '#000000', 0, 0, 0, {safe_value(row['is_archive'])}, NULL, 0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, 'From_InvoiceHeadSummary');
+        """
+        target_cursor.execute(query)
+    except Exception as e:
+        logging.error(f"Error inserting row {index}: {e}")
+        break
+
+myconnection.commit()
+myconnection.close()
+bar.close()
+print('Hospitals data inserted successfully.')
